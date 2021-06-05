589ccb145cadedfd375e0ccbc00f8896ff79b538
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-06-04 20:15:31 -0400

    deleted hover.js and fixed link to html

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 var myAnimation = new hoverEffect({
             parent: document.querySelector('.img'),
-            intensity: 0.3,
+            intensity: 0.4,
             image1: './assets/dye1.jpg',
             image2: './assets/dye2.jpg',
             displacementImage: './assets/dis.png'
 });
+
+
+
+
+
+
         
