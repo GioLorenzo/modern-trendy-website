c5cd0640cc5e22a89b2bbf6c4a500e8404fdf7ab
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-06-04 20:07:49 -0400

    javascript

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var myAnimation = new hoverEffect({
+            parent: document.querySelector('.img'),
+            intensity: 0.3,
+            image1: './assets/dye1.jpg',
+            image2: './assets/dye2.jpg',
+            displacementImage: './assets/dis.png'
+});
+        
